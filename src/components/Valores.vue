53abac67569f502098af05421b1cfd8174ceb58a
satbh <carlosarcia1827@gmail.com>
2021-11-15 04:34:27 -0600

    multiples valores a partir de valores.js

--- src/components/Valores.vue
+++ src/components/Valores.vue
@@ -1,17 +1,18 @@
 <template>
-	<div class="flex-colum gap-4">
-		<Valor title="some" text="bar"/>
-	</div>
+  <Valor v-for="valor in valores"
+         :title="valor.title"
+         :text="valor.text"
+         :key="valor"/>
 </template>
 
 <script> 
   import Valor from './Valor.vue'
-//  import valores from './valores.js'
+  import valores from './valores.js'
   export default {
     components: {Valor},
     data() {
       return {
-
+        valores
       }
     }
   }
