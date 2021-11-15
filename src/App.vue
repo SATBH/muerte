bd96d98fd8fe942205088d717c753ed03c1ef5bc
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-11-15 08:34:59 -0300

    Fixed app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <NavBar/>
-  <router-view/>
+	<main class="container mx-auto my-8 max-w-4xl">
+		<router-view/>
+	</main>
 </template>
 
 
@@ -16,23 +18,6 @@ export default {
 </script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: 'Inter', sans-serif;
 }
 </style>
