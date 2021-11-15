75e392dd45be86a47d51701b4b152fd552ee7c5f
satbh <carlosarcia1827@gmail.com>
2021-11-15 07:30:14 -0600

    algo algo

--- src/components/QuienesSomos.vue
+++ src/components/QuienesSomos.vue
@@ -10,7 +10,7 @@
 	-->
   <h2 class="text-center text-2xl font-bold uppercase mt-8 mb-4">Fundadores</h2>
 	<ul class="space-y-4">
-		<li class="flex-column space-y-4 sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200">
+		<li class="fcursor-pointer transition hover:shadow-md lex-column space-y-4 sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200">
 			<div class="w-44 h-44 mx-auto bg-blue-500 rounded-full"></div>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Brian Eduardo Arias Perez</h3>
@@ -20,7 +20,7 @@
 				reprehenderit quidem dolor?lorem </p>
 			</div>
 		</li>
-		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200">
+		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
 			<div class="w-44 h-44 mx-auto bg-blue-500 rounded-full"></div>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Carlos Fernando Arcia Castro</h3>
@@ -30,8 +30,8 @@
 				reprehenderit quidem dolor?lorem </p>
 			</div>
 		</li>
-		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200">
-			<div class="w-44 h-44 mx-auto bg-blue-500 rounded-full"></div>
+		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
+			<img class="object-cover w-44 h-44 rounded-full" src="@/assets/bryan.jpeg"/>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Bryan Josué Martínez Espinoza</h3>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
---
+++ src/components/]
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <div class="p-8 text-center text-gray-600 text-sm max-w-4xl mx-auto border-t-2 border-gray-200">
+        <div class="flex justify-center gap-4">
+          <div>
+            Correo: solarsolutions248@gmail.com  
+          </div>
+          -
+          <div> 
+            Numero de telefono: 82131992
+          </div>
+        </div>
+      </div>
+  </footer>
+</template>
