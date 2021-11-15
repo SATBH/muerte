298419525955809d161cb84eb27173d3b719ff20
satbh <carlosarcia1827@gmail.com>
2021-11-15 07:30:42 -0600

    nada nada

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@
 	<main class="container max-w-4xl mx-auto my-8 px-4 sm:px-8">
 		<router-view/>
 	</main>
+  <Footer/>
 </template>
 
 
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 export default {
-  components: {NavBar},
+  components: {NavBar, Footer},
   data(){
     return {}
   }
