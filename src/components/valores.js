a099c8b202f57e80e46052ebf24d55408ffaae7b
satbh <carlosarcia1827@gmail.com>
2021-11-15 03:56:47 -0600

    creando json de valores

---
+++ src/components/valores.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    image: "",
+    title: "Respeto y responsabilidad",
+    text: `Creemos en que el respeto es uno de las valores mas fundamentales
+    para crear una gran empresa. Ya sea entre trabajadores como con los clientes`
+  },
+  {
+    image: "",
+    title: "Calidad y eficiencia en cada servicio",
+    text: `Intentamos siempre brindar la mejor calidad posible para nuestros clientes`
+  },
+  {
+    image: "",
+    title: "Trabajo en equipo",
+    text: `Intentamos siempre brindar la mejor calidad posible para nuestros clientes`
+  }
+]
