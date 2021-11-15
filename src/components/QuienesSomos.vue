14df1d3a98d5a6737cd776ede9152ebeec10806c
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-11-15 11:10:22 -0300

    Added cursor-pointer

--- src/components/QuienesSomos.vue
+++ src/components/QuienesSomos.vue
@@ -8,9 +8,9 @@
 	<!--
   <img class="mx-auto my-4 w-64" src="@/assets/Logo2.jpeg"/>
 	-->
-  <h2 class="text-center text-2xl font-bold uppercase mt-8 mb-4">Fundadores</h2>
+  <h2 class="text-center text-2xl font-bold mt-8 mb-4">Fundadores</h2>
 	<ul class="space-y-4">
-		<li class="fcursor-pointer transition hover:shadow-md lex-column space-y-4 sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200">
+		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
 			<img class="object-cover w-44 h-44 rounded-full" src="@/assets/brian.jpeg"/>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Brian Eduardo Arias Perez</h3>
