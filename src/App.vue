ee732e11bb32d921bd1eea690c7b1912dd1bf019
satbh <carlosarcia1827@gmail.com>
2021-11-15 04:48:27 -0600

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <NavBar/>
-  <div class="container w-full max-w-3xl mx-auto my-8 px-8">
-    <Valores/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
-
-<script>
-import NavBar from './components/NavBar.vue'
-//import Mision from './components/Mision.vue'
-import Valores from './components/Valores.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavBar,
-   // Mision,
-    Valores
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: 'Inter', sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
