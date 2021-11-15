5b53c8034621d5da19a3c891811238d22ea9f56b
satbh <carlosarcia1827@gmail.com>
2021-11-15 07:32:07 -0600

    fua chaval

--- src/components/QuienesSomos.vue
+++ src/components/QuienesSomos.vue
@@ -11,7 +11,7 @@
   <h2 class="text-center text-2xl font-bold uppercase mt-8 mb-4">Fundadores</h2>
 	<ul class="space-y-4">
 		<li class="fcursor-pointer transition hover:shadow-md lex-column space-y-4 sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200">
-			<div class="w-44 h-44 mx-auto bg-blue-500 rounded-full"></div>
+			<img class="object-cover w-44 h-44 rounded-full" src="@/assets/brian.jpeg"/>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Brian Eduardo Arias Perez</h3>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
