d0f093752cb42e2fb00136f43de7427dc42ea2f6
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-11-15 11:08:03 -0300

    Added social links

--- src/components/Contactenos.vue
+++ src/components/Contactenos.vue
@@ -1,13 +1,13 @@
 <template>
   <ul>
     <li>
-      <a> https://www.facebook.com/Solar-Solutions-110321521444582 </a>
+      <a href="https://www.facebook.com/Solar-Solutions-110321521444582">Facebook</a>
     </li>
     <li>
-      <a> https://twitter.com/SolarSol23 </a>
+      <a href="https://twitter.com/SolarSol23">Twitter</a>
     </li>
     <li>
-      <a> https://www.instagram.com/solarsolutions23/?hl=es </a>
+      <a href="https://www.instagram.com/solarsolutions23/?hl=es">Instagram</a>
     </li>
   </ul>
 </template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,22 @@
 <template>
-  <footer>
-    <div class="p-8 text-center text-gray-600 text-sm max-w-4xl mx-auto border-t-2 border-gray-200">
-        <div class="flex justify-center gap-4">
-          <div>
-            Correo: solarsolutions248@gmail.com  
-          </div>
-          -
-          <div> 
-            Numero de telefono: 82131992
-          </div>
-        </div>
-      </div>
-  </footer>
+	<footer class="p-8 text-gray-600 text-sm max-w-4xl mx-auto border-t-2 border-gray-200 space-y-4">
+		<div class="flex justify-center gap-4">
+			<div>Correo: solarsolutions248@gmail.com</div>
+			-
+			<div>Numero de telefono: 82131992</div>
+		</div>
+		<ul class="flex justify-center gap-4">
+			<li>
+				<a href="https://www.facebook.com/Solar-Solutions-110321521444582" class="underline">Facebook</a>
+			</li>
+			•
+			<li>
+				<a href="https://twitter.com/SolarSol23" class="underline">Twitter</a>
+			</li>
+			•
+			<li>
+				<a href="https://www.instagram.com/solarsolutions23/?hl=es" class="underline">Instagram</a>
+			</li>
+		</ul>
+	</footer>
 </template>
