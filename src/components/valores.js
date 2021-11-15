daada7b25037251b8fb66b7d6601aad2a69a6108
satbh <carlosarcia1827@gmail.com>
2021-11-15 04:14:45 -0600

    descripcion de valores

--- src/components/valores.js
+++ src/components/valores.js
@@ -1,18 +1,31 @@
 export default [
   {
-    image: "",
     title: "Respeto y responsabilidad",
     text: `Creemos en que el respeto es uno de las valores mas fundamentales
     para crear una gran empresa. Ya sea entre trabajadores como con los clientes`
   },
   {
-    image: "",
     title: "Calidad y eficiencia en cada servicio",
     text: `Intentamos siempre brindar la mejor calidad posible para nuestros clientes`
   },
   {
-    image: "",
     title: "Trabajo en equipo",
-    text: `Intentamos siempre brindar la mejor calidad posible para nuestros clientes`
+    text: `En Solar Solutions creemos que la coordinacion y el buen trabajo en 
+    equipo son esenciales para hacer un buen trabajo.`
+  },
+  {
+    title: "Sinceridad",
+    text: `Nos enorgullecemos de ser una empresa honesta y que antepone sus 
+    valores a sus intereses.`
+  },
+  {
+    title: "Autosuperacion",
+    text: `Buscamos mejorar constantemente, actualizando nuestros servicios y 
+    productos`
+  },
+  {
+    title: "Capacitados",
+    text: `Cada area de trabajo de la empresa goza de tener personal capacitado
+    en esta.`
   }
 ]
