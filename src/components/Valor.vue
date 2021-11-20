8fadc76ac70bb5d7e36b0209f1ef6eec3951458b
satbh <carlosarcia1827@gmail.com>
2021-11-20 11:11:34 -0600

    mal codigo

--- src/components/Valor.vue
+++ src/components/Valor.vue
@@ -1,5 +1,5 @@
 <template>
-	<div  class="valor p-4 rounded-lg border-2 border-gray-200 space-y-2 cursor-pointer transition hover:shadow-md">
+	<div :style="`animation-delay: ${number * 300}ms`" class="valor p-4 rounded-lg border-2 border-gray-200 space-y-2 cursor-pointer transition hover:shadow-md">
 		<h2 class="text-lg font-bold">{{title}}</h2>
 		<p>{{text}}</p>
 	</div>
@@ -23,12 +23,19 @@ export default {
 <style scoped>
 .valor {
 	animation-name: slidein;
-	animation-duration: 1s;
+	animation-duration: 0.7s;
+	animation-fill-mode: forwards;
+	opacity: 0;
 }
 
 @keyframes slidein {
   from {
-		transform: translateY(-20px);
+		transform: translateY(-40px);
+		opacity: 0;
   }
+
+	to {
+		opacity: 1;
+	}
 }
 </style>
