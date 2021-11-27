e7fb23ec8a68639b7106a4fd826b35c7bd189bdc
satbh <carlosarcia1827@gmail.com>
2021-11-26 18:03:41 -0600

    baia baia

--- src/components/QuienesSomos.vue
+++ src/components/QuienesSomos.vue
@@ -15,7 +15,7 @@
     </p>
   <h2 class="text-center text-2xl font-bold mt-8 mb-4">Fundadores</h2>
 	<ul class="space-y-4">
-		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
+		<li class="flex-column sm:flex gap-4 p-4 rounded-md border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
 			<img class="object-cover w-44 h-44 rounded-full m-auto" src="@/assets/brian.jpeg"/>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Brian Eduardo Arias Perez</h3>
@@ -25,7 +25,7 @@
 				reprehenderit quidem dolor?lorem </p>
 			</div>
 		</li>
-		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
+		<li class="flex-column sm:flex gap-4 p-4 rounded-md border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
 			<img class="object-cover w-44 h-44 rounded-full m-auto" src="@/assets/carlos.png"/>
 			<div class="flex-1 my-auto space-y-2">
 
@@ -36,7 +36,7 @@
 				reprehenderit quidem dolor?lorem </p>
 			</div>
 		</li>
-		<li class="flex-column sm:flex gap-4 p-4 rounded-lg border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
+		<li class="flex-column sm:flex gap-4 p-4 rounded-md border-2 border-gray-200 cursor-pointer transition hover:shadow-md">
 			<img class="object-cover w-44 h-44 rounded-full m-auto" src="@/assets/bryan.jpeg"/>
 			<div class="flex-1 my-auto space-y-2">
 				<h3 class="text-lg text-center sm:text-left font-medium">Bryan Josué Martínez Espinoza</h3>
--- src/components/Valor.vue
+++ src/components/Valor.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :style="`animation-delay: ${number * 300}ms`" class="valor p-4 rounded-lg border-2 border-gray-200 space-y-2 cursor-pointer transition hover:shadow-md">
+	<div :style="`animation-delay: ${number * 200}ms`" class="valor p-4 rounded-lg border-2 border-gray-200 space-y-2 cursor-pointer transition hover:shadow-md">
 		<h2 class="text-lg font-bold">{{title}}</h2>
 		<p>{{text}}</p>
 	</div>
@@ -23,7 +23,7 @@ export default {
 <style scoped>
 .valor {
 	animation-name: slidein;
-	animation-duration: 0.7s;
+	animation-duration: 0.5s;
 	animation-fill-mode: forwards;
 	opacity: 0;
 }
